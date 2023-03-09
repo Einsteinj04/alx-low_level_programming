ed393b6fef1586a5987f2051a10aeabf8879df39
Einsteinj04 <jilchrisdascientist@gmail.com>
2023-03-09 10:47:44 -0800

    commited

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-
 /*
  *main- learning c language
 */
 
 int main(void)
 {
-    char ch;
-    ch = 'a';
+char ch;   
+ch = 'a';
 
-    while (ch <= 'z')
-    {
-     putchar(ch);
-     ch++;
-    }
-    putchar('\n');
-    return (0);
+while (ch <= 'z')
+{
+ putchar(ch);
+ ch++;
+}
+putchar('\n');
+return (0);
 }
